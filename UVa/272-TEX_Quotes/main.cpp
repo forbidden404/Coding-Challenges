f5951cf5fff1bf590c09dd3e649152dde77b0c8a
Francisco Soares <francisco.ssn94@gmail.com>
2019-05-09 17:13:37 -0300

    Add UVa 272 - TEX Quotes

--- UVa/272-TEX_Quotes/main.cpp
+++ UVa/272-TEX_Quotes/main.cpp
@@ -3,28 +3,22 @@
 using namespace std;
 
 int main() {
+    int turn = 0;
     string line;
 
-    bool isFirstQuote = true;
-    bool isFirstLine = true;
     while(getline(cin, line)) {
-        if (!isFirstLine) cout << "\n";
-        for (auto c : line) {
-            if (c == '"') {
-                if (isFirstQuote) {
-                    isFirstQuote = false;
-                    cout << "``";
-                } else {
-                    isFirstQuote = true;
-                    cout << "''";
-                }
+        size_t found = -1;
+        // Read line by line and look for quotes, if it's first quote, replace with ``, otherwise replace with ''
+        while (found = line.find("\"", found + 1), (found != string::npos)) { 
+            if (!turn) {
+                line.replace(found, 1, "``");
             } else {
-                cout << c;
+                line.replace(found, 1, "''");
             }
+            turn = (turn + 1) % 2;
         }
-        isFirstLine = false;
+        cout << line << "\n";
     }
-    cout << "\n";
 
     return 0;
 }
