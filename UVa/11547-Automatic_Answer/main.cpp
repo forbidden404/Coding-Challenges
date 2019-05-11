35ab33eb4a1a66ba2a24a64ba092abcdd5269de3
Francisco Soares <francisco.ssn94@gmail.com>
2019-05-11 02:11:39 -0300

    Change multiple operations to one line in Automatic Answer

--- UVa/11547-Automatic_Answer/main.cpp
+++ UVa/11547-Automatic_Answer/main.cpp
@@ -9,16 +9,8 @@ int main() {
     while (t--) {
         long long n;
         cin >> n;
-
-        n *= 567;
-        n /= 9;
-        n += 7492;
-        n *= 235;
-        n /= 47;
-        n -= 498;
         
-        n /= 10;
-        cout << abs(n % 10) << endl;
+        cout << abs((((((((n * 567) / 9) + 7492) * 235) / 47) - 498) / 10) % 10) << endl;
     }
 
     return 0;
